d5ef9dd18bca339584f76fdc7b0479cec91d6543
lgm <3113778748@qq.com>
2022-07-29 17:52:16 +0800

    hot_fix three commit

--- src/main/java/com/git/ftghy.java
+++ src/main/java/com/git/ftghy.java
@@ -4,6 +4,6 @@ public class ftghy {
     public static void main(String[] args) {
         System.out.println("123456789");
         System.out.println("55555555");
-        System.out.println("master");
+        System.out.println("hoy_fix第三次修改");
     }
 }
