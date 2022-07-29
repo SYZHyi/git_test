a1736337310a42989e67d6ff9d087800e2cde218
lgm <3113778748@qq.com>
2022-07-29 17:07:27 +0800

    hot-fix two commit

--- src/main/java/com/git/ftghy.java
+++ src/main/java/com/git/ftghy.java
@@ -4,6 +4,6 @@ public class ftghy {
     public static void main(String[] args) {
         System.out.println("123456789");
         System.out.println("55555555");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix第二次提交");
     }
 }
