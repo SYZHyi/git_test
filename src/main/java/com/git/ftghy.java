cfca503baecc7fad7ae8ab3bbe547e747fbffde2
SYZHyi <110174006+SYZHyi@users.noreply.github.com>
2022-07-29 20:00:12 +0800

    Update ftghy.java

--- src/main/java/com/git/ftghy.java
+++ src/main/java/com/git/ftghy.java
@@ -6,5 +6,6 @@ public class ftghy {
         System.out.println("55555555");
         System.out.println("master第三次提交");
         System.out.println("hoy_fix第三次修改");
+        System.out.println("在github上修改");
     }
 }
